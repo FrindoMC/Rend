87192e39eecda9ab74422947fc05ab360e268841
Frindo <48627170+FrindoMC@users.noreply.github.com>
2024-08-22 05:23:31 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,9 +143,5 @@ tasks.shadowJar {
     configurations = emptyList()
 }
 
-    // If you want to include other dependencies and shadow them, you can relocate them in here
-    fun relocate(name: String) = relocate(name, "$baseGroup.deps.$name")
-}
-
 tasks.assemble.get().dependsOn(tasks.remapJar)
 
